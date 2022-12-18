3d0b1ae9996d364138a468e1e803468e664b1ac7
Mitesh <mitesh.vishwakarma@gmail.com>
2022-12-18 11:57:53 +0530

    testing log

--- src/main/java/com/lld/app/registercallback/RegisterCallbackApplication.java
+++ src/main/java/com/lld/app/registercallback/RegisterCallbackApplication.java
@@ -1,14 +1,16 @@
 package com.lld.app.registercallback;
 
 import com.lld.app.registercallback.task.SimpleTask;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * A Class to register a callback function to the system which is invoked the end system itself.
  */
+@Slf4j
 public class RegisterCallbackApplication {
 
     public void mainApplication() {
         SimpleTask task = new SimpleTask();
-        task.registerCallback(() -> System.out.println("Callback function executing"));
+        task.registerCallback(() -> log.info("Callback function executing"));
     }
 }
